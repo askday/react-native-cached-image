7c717bde6b328376dee8c26c31e90de15e4e6b53
史晓霞 <dx910114@gmail.com>
2019-03-01 19:11:08 +0800

    MOD：升级rn至0.58.4

--- CachedImageExample/index.js
+++ CachedImageExample/index.js
@@ -6,170 +6,171 @@ const ReactNative = require('react-native');
 const _ = require('lodash');
 
 const {
-    View,
-    ScrollView,
-    Button,
-    Dimensions,
-    StyleSheet,
-    AppRegistry,
-    ListView,
+  View,
+  ScrollView,
+  Button,
+  Dimensions,
+  StyleSheet,
+  AppRegistry,
+  FlatList,
+  Alert
 } = ReactNative;
 
 const {
-    CachedImage,
-    ImageCacheProvider,
-    ImageCacheManager,
+  CachedImage,
+  ImageCacheProvider,
+  ImageCacheManager,
 } = require('react-native-cached-image');
 
 const {
-    width
+  width
 } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 20
-    },
-    buttons: {
-        flexDirection: 'row'
-    },
-    button: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image: {
-        width,
-        height: 300
-    }
+  container: {
+    flex: 1,
+    marginTop: 20
+  },
+  buttons: {
+    flexDirection: 'row'
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width,
+    height: 300,
+    marginBottom: 10,
+  }
 });
 
 const loading = require('./loading.jpg');
 
-const image1 = 'https://wallpaperbrowse.com/media/images/bcf39e88-5731-43bb-9d4b-e5b3b2b1fdf2.jpg';
-const image2 = 'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/baby-shower-full.jpg';
+const image1 = 'https://b-ssl.duitang.com/uploads/blog/201502/14/20150214130013_eGV2m.thumb.1000_0.jpeg';
+const image2 = 'https://b-ssl.duitang.com/uploads/item/201402/14/20140214120558_2f4NN.thumb.1300_0.jpeg';
 
 const images = [
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/after-work-drinks-full.jpg',
-    'https://i.ytimg.com/vi/b6m-XlOxjbk/hqdefault.jpg',
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/wrong-image.jpg',
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/bar-crawl-full.jpg',
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/cheeseburger-full.jpg',
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/friendsgiving-full.jpg',
-    'https://d22cb02g3nv58u.cloudfront.net/0.676.0/assets/images/icons/fun-types/full/dogs-play-date-full.jpg'
+  'https://b-ssl.duitang.com/uploads/blog/201502/14/20150214130010_wYAAK.thumb.1000_0.jpeg',
+  'https://b-ssl.duitang.com/uploads/item/201306/18/20130618092329_4APwX.thumb.1300_0.jpeg',
+  'https://b-ssl.duitang.com/uploads/item/201307/30/20130730144824_GTnmP.thumb.1000_0.jpeg',
+  'https://b-ssl.duitang.com/uploads/item/201307/30/20130730144823_twFMU.thumb.1000_0.jpeg',
+  'https://b-ssl.duitang.com/uploads/item/201307/30/20130730144726_tfHJV.thumb.1000_0.jpeg',
+  'https://b-ssl.duitang.com/uploads/item/201307/30/20130730144759_E4GuJ.thumb.1000_0.jpeg',
 ];
 
 function formatBytes(bytes, decimals) {
-    if (bytes === 0) {
-        return '0 B';
-    }
-    const k = 1000;
-    const dm = decimals + 1 || 3;
-    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  if (bytes === 0) {
+    return '0 B';
+  }
+  const k = 1000;
+  const dm = decimals + 1 || 3;
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
 const defaultImageCacheManager = ImageCacheManager();
 
 class CachedImageExample extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            showNextImage: false,
-            dataSource: ds.cloneWithRows(images)
-        };
+    this.state = {
+      showNextImage: false,
+      dataSource: images
+    };
 
-        this.cacheImages = this.cacheImages.bind(this);
+    this.cacheImages = this.cacheImages.bind(this);
 
-    }
+  }
 
-    componentWillMount() {
-        defaultImageCacheManager.downloadAndCacheUrl(image1);
-    }
+  componentWillMount() {
+    defaultImageCacheManager.downloadAndCacheUrl(image1);
+  }
 
-    clearCache() {
-        defaultImageCacheManager.clearCache()
-            .then(() => {
-                ReactNative.Alert.alert('Cache cleared');
-            });
-    }
+  clearCache() {
+    defaultImageCacheManager.clearCache()
+      .then(() => {
+        Alert.alert('Cache cleared');
+      });
+  }
 
-    getCacheInfo() {
-        defaultImageCacheManager.getCacheInfo()
-            .then(({size, files}) => {
-                // console.log(size, files);
-                ReactNative.Alert.alert('Cache Info', `files: ${files.length}\nsize: ${formatBytes(size)}`);
-            });
-    }
+  getCacheInfo() {
+    defaultImageCacheManager.getCacheInfo()
+      .then(({ size, files }) => {
+        // console.log(size, files);
+        Alert.alert('Cache Info', `files: ${files.length}\nsize: ${formatBytes(size)}`);
+      });
+  }
 
-    cacheImages() {
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows([])
-        }, () => {
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(images)
-            });
-        });
-    }
+  cacheImages() {
+    this.setState({
+      dataSource: []
+    }, () => {
+      this.setState({
+        dataSource: images
+      });
+    });
+  }
 
-    renderRow(uri) {
-        return (
-            <CachedImage
-                source={{uri}}
-                defaultSource={loading}
-                style={styles.image}
-            />
-        );
-    }
+  renderRow({ item }) {
+    return (
+      <CachedImage
+        source={{ uri: item }}
+        defaultSource={loading}
+        style={styles.image}
+      />
+    );
+  }
 
-    render() {
-        return (
-            <ScrollView style={styles.container}>
-                <View style={styles.buttons}>
-                    <Button
-                        onPress={this.clearCache}
-                        title="Clear Cache"
-                        color="#6f97e5"
-                    />
-                    <Button
-                        onPress={this.getCacheInfo}
-                        title="Cache Info"
-                        color="#2ce7cc"
-                    />
-                    <Button
-                        onPress={this.cacheImages}
-                        title="Cache Images"
-                        color="#826fe5"
-                    />
-                </View>
-                <View>
-                    <CachedImage
-                        source={{uri: image1}}
-                        style={styles.image}
-                    />
-                    <CachedImage
-                        source={{uri: image2}}
-                        style={styles.image}
-                    />
-                </View>
-                <ImageCacheProvider
-                    urlsToPreload={images}
-                    onPreloadComplete={() => ReactNative.Alert.alert('onPreloadComplete')}
-                    ttl={60}
-                    numberOfConcurrentPreloads={0}>
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={this.renderRow}
-                        enableEmptySections={true}
-                    />
-                </ImageCacheProvider>
-            </ScrollView>
-        );
-    }
+  render() {
+    const { dataSource } = this.state;
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.buttons}>
+          <Button
+            onPress={this.clearCache}
+            title="Clear Cache"
+            color="#6f97e5"
+          />
+          <Button
+            onPress={this.getCacheInfo}
+            title="Cache Info"
+            color="#2ce7cc"
+          />
+          <Button
+            onPress={this.cacheImages}
+            title="Cache Images"
+            color="#826fe5"
+          />
+        </View>
+        <View>
+          <CachedImage
+            source={{ uri: image1 }}
+            style={styles.image}
+          />
+          <CachedImage
+            source={{ uri: image2 }}
+            style={styles.image}
+          />
+        </View>
+        <ImageCacheProvider
+          urlsToPreload={images}
+          onPreloadComplete={() => Alert.alert('onPreloadComplete')}
+          ttl={60}
+          numberOfConcurrentPreloads={0}>
+          <FlatList
+            keyExtractor={(item, index) => `${index} - ${item}`}
+            data={dataSource}
+            renderItem={this.renderRow}
+          />
+        </ImageCacheProvider>
+      </ScrollView>
+    );
+  }
 
 }
 
